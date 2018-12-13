90646163884e8ecf3833346c41420a1ba6ff6a47
Mariya Kulakova <mari.kulakova98@mail.ru>
2018-12-13 16:28:21 +0300

    ver 1.2.1

--- game/main.cpp
+++ game/main.cpp
@@ -219,7 +219,7 @@ void checkCollisionWithMap(float Dx, float Dy)//ф-ция проверки столкновений с ка
 		dx = speed;
 		CurrentFrame += 0.005*time;
 		if (CurrentFrame > 3) CurrentFrame -= 3;
-		sprite.setTextureRect(IntRect(60 * int(CurrentFrame), 120, 50, 50));
+		sprite.setTextureRect(IntRect(50 * int(CurrentFrame), 120, 50, 50));
 		break;
 		}
 		case 1:{//состояние идти влево
@@ -258,6 +258,99 @@ void checkCollisionWithMap(float Dx, float Dy)//ф-ция проверки столкновений с ка
 		}
 	}
 };//класс Enemy закрыт
+
+//////////////////////sozdanie HardEnemy//////////
+class Enemy :public Entity{
+public:
+	int direction;//направление движения врага
+	Enemy(Image &image, float X, float Y, int W, int H, std::string Name) :Entity(image, X, Y, W, H, Name){
+	if (name == "HardEnemy"){
+		//Задаем спрайту один прямоугольник для
+		//вывода одного игрока. IntRect – для приведения типов
+		sprite.setTextureRect(IntRect(0, 0, w, h));
+		direction = rand() % (3); //Направление движения врага задаём случайным образом
+			//через генератор случайных чисел
+			speed = 0.1;//даем скорость.этот объект всегда двигается
+			dx = speed;
+		}
+	}
+
+void checkCollisionWithMap(float Dx, float Dy)//ф-ция проверки столкновений с картой
+	{
+	for (int i = y / 32; i < (y + h) / 32; i++)//проходимся по элементам карты
+		for (int j = x / 32; j<(x + w) / 32; j++)
+		{
+			if (TileMap[i][j] == '0')//если элемент - тайлик земли
+			{
+				if (Dy > 0) {
+					y = i * 32 - h;  dy = -0.1; 
+					direction = rand() % (3); //Направление движения врага
+						}//по Y 
+				if (Dy < 0) {
+					y = i * 32 + 32; dy = 0.1; 
+					direction = rand() % (3);//Направление движения врага 
+						}//столкновение с верхними краями 
+				if (Dx > 0) {
+					x = j * 32 - w; dx = -0.1; 
+						direction = rand() % (3);//Направление движения врага 
+						}//с правым краем карты
+				if (Dx < 0) {
+					x = j * 32 + 32; dx = 0.1; 
+						direction = rand() % (3); //Направление движения врага
+						}// с левым краем карты
+				}
+			}
+	}
+
+	void update(float time)
+	{
+		if (name == "HardEnemy"){//для персонажа с таким именем логика будет такой
+
+		if (life) {//проверяем, жив ли герой
+		switch (direction)//делаются различные действия в зависимости от состояния
+		{
+		case 0:{//состояние идти вправо
+		dx = speed;
+		CurrentFrame += 0.005*time;
+		if (CurrentFrame > 3) CurrentFrame -= 3;
+		sprite.setTextureRect(IntRect(50 * int(CurrentFrame), 120, 50, 50));
+		break;
+		}
+		case 1:{//состояние идти влево
+		dx = -speed;
+		CurrentFrame += 0.005*time;
+		if (CurrentFrame > 3) CurrentFrame -= 3;
+		sprite.setTextureRect(IntRect(50 * int(CurrentFrame), 60, 50, 60));
+		break;
+		}
+		case 2:{//идти вверх
+		dy = -speed;
+		CurrentFrame += 0.005*time;
+		if (CurrentFrame > 3) CurrentFrame -= 3;
+		sprite.setTextureRect(IntRect(50 * int(CurrentFrame), 180, 50, 60));
+		break;
+		}
+		case 3:{//идти вниз
+		dy = speed;
+		CurrentFrame += 0.005*time;
+		if (CurrentFrame > 3) CurrentFrame -= 3;
+		sprite.setTextureRect(IntRect(50 * int(CurrentFrame), 0, 50, 60));
+		break;
+		}
+		}
+
+		x += dx*time; //движение по “X”
+		checkCollisionWithMap(dx, 0);//обрабатываем столкновение по Х
+
+		y += dy*time; //движение по “Y”
+		checkCollisionWithMap(0, dy);//обрабатываем столкновение по Y
+
+		sprite.setPosition(x, y); //спрайт в позиции (x, y).
+
+		if (health <= 0){ life = false; }//если жизней меньше 0, либо равно 0, то умираем
+		}
+		}
+	};//class close
 
 
 ////////////////////////////КЛАСС ПУЛИ////////////////////////
@@ -323,7 +416,7 @@ int main()
 	text.setColor(Color::Red);//покрасили текст в красный	text.setStyle(Text::Bold);//жирный текст.
 
 	Image map_image;//объект изображения для карты
-	map_image.loadFromFile("images/map.png");//загружаем файл для карты
+	map_image.loadFromFile("images/map_new.png");//загружаем файл для карты
 	Texture map;//текстура карты
 	map.loadFromImage(map_image);//заряжаем текстуру картинкой
 	Sprite s_map;//создаём спрайт для карты
@@ -339,6 +432,9 @@ int main()
 
 	Image easyEnemyImage;
 	easyEnemyImage.loadFromFile("images/skel.png"); // загружаем изображение врага
+
+	Image hardEnemyImage;
+	hardEnemyImage.loadFromFile("images/mob.png"); // добавила изображение хард врага
 
 	Image BulletImage;//изображение для пули
 	BulletImage.loadFromFile("images/bullet.png");//загрузили картинку в объект изображения

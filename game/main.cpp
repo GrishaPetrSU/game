83949e7f8868876d3191b6bb26593084d8c0b69c
Edigarev <griha.edigarev@gmail.com>
2018-12-19 15:25:05 +0300

    add menu

--- game/main.cpp
+++ game/main.cpp
@@ -311,12 +311,59 @@ public:
 	}
 };
 
+///////////////////////////////creat MENUSSS))(
+void menu(RenderWindow & window) {
+	Texture menuTexture1, menuTexture2, menuTexture3, aboutTexture, menuBackground;
+	menuTexture1.loadFromFile("images/new_game.png");
+	menuTexture2.loadFromFile("images/about_game.png");
+	menuTexture3.loadFromFile("images/exit.png");
+	aboutTexture.loadFromFile("images/about.png");
+	menuBackground.loadFromFile("images/menu.jpg");
+	Sprite menu1(menuTexture1), menu2(menuTexture2), menu3(menuTexture3), about(aboutTexture), menuBg(menuBackground);
+	bool isMenu = 1;
+	int menuNum = 0;
+	menu1.setPosition(100, 30);
+	menu2.setPosition(100, 90);
+	menu3.setPosition(100, 150);
+	menuBg.setPosition(345, 0);
+ 
+	//////////////////////////////МЕНЮ///////////////////
+	while (isMenu)
+	{
+		menu1.setColor(Color::White);
+		menu2.setColor(Color::White);
+		menu3.setColor(Color::White);
+		menuNum = 0;
+		window.clear(Color(129, 181, 221));
+ 
+		if (IntRect(100, 30, 300, 50).contains(Mouse::getPosition(window))) { menu1.setColor(Color::Blue); menuNum = 1; }
+		if (IntRect(100, 90, 300, 50).contains(Mouse::getPosition(window))) { menu2.setColor(Color::Blue); menuNum = 2; }
+		if (IntRect(100, 150, 300, 50).contains(Mouse::getPosition(window))) { menu3.setColor(Color::Blue); menuNum = 3; }
+ 
+		if (Mouse::isButtonPressed(Mouse::Left))
+		{
+			if (menuNum == 1) isMenu = false;//если нажали первую кнопку, то выходим из меню 
+			if (menuNum == 2) { window.draw(about); window.display(); while (!Keyboard::isKeyPressed(Keyboard::Escape)); }
+			if (menuNum == 3)  { window.close(); isMenu = false; }
+ 
+		}
+ 
+		window.draw(menuBg);
+		window.draw(menu1);
+		window.draw(menu2);
+		window.draw(menu3);
+		
+		window.display();
+	}
+	////////////////////////////////////////////////////
+}
 
 
 int main()
 {
 	sf::VideoMode desktop = sf::VideoMode::getDesktopMode();
-	sf::RenderWindow window(sf::VideoMode(800, 640, desktop.bitsPerPixel), "Skeleton");
+	sf::RenderWindow window(sf::VideoMode(800, 600, desktop.bitsPerPixel), "Skeleton");
+	menu(window);
 
 	Font font;//шрифт 
 	font.loadFromFile("ttf/CyrilicOld.ttf");//передаем нашему шрифту файл шрифта
